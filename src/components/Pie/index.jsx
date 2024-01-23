58bdb38b0fd1f0d8cd450dd078cf11ed5a329d82
Cenoy21 <ceno7517@gmail.com>
2024-01-23 18:20:10 +0530

    Fetch pie chart data from backend; Replace static data in the Pie component with dynamic data fetched from the '/api/pie-chart' endpoint. Updated the component to use Axios for making asynchronous requests. This enhances the PieChart component's flexibility by allowing it to dynamically display data from the server.

--- src/components/Pie/index.jsx
+++ src/components/Pie/index.jsx
@@ -1,7 +1,41 @@
 import "./pie.css";
+import { PieChart } from "@mui/x-charts/PieChart";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Pie = () => {
-  return <></>;
+  const [pieData, setPieData] = useState([]);
+
+  const fetchPieData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/pie-chart");
+      setPieData(response.data);
+    } catch (e) {
+      console.error("Error fetching pie chart data:", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchPieData();
+  }, []);
+
+  return (
+    <>
+      <PieChart
+        series={[
+          {
+            data: pieData.map((item, index) => ({
+              id: index,
+              value: item.value,
+              label: item.label,
+            })),
+          },
+        ]}
+        width={400}
+        height={200}
+      />
+    </>
+  );
 };
 
 export default Pie;
