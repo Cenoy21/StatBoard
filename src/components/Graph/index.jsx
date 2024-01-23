9d847e055bb4d16c8fa00afa09001d3e83a4899c
Cenoy21 <ceno7517@gmail.com>
2024-01-23 19:53:52 +0530

    Integrated Material UI Graph component and displayed the values from the backend

--- src/components/Graph/index.jsx
+++ src/components/Graph/index.jsx
@@ -1,7 +1,37 @@
 import "./graph.css";
+import { useState, useEffect } from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+import axios from "axios";
 
 const Graph = () => {
-  return <></>;
+  const [graphData, setGraphData] = useState([]);
+
+  const fetchGraphData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/graph");
+      setGraphData(response.data);
+    } catch (e) {
+      console.error("Error fetching Graph data:", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchGraphData();
+  }, []);
+
+  const xData = graphData.map((item) => item.x);
+  const data = graphData.map((item) => item.y);
+
+  return (
+    <>
+      <LineChart
+        xAxis={[{ data: xData, scaleType: "point", labelRotate: -45 }]}
+        series={[{ data, connectNulls: true }]}
+        height={300}
+        width={400}
+      />
+    </>
+  );
 };
 
 export default Graph;
