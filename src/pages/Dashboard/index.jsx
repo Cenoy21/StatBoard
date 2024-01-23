4d2a9362f9196decae9b2f8df1819c4e03bf9de7
Cenoy21 <ceno7517@gmail.com>
2024-01-23 23:23:36 +0530

    completed the sidebar, also implemented links

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -10,11 +10,33 @@ const Dashboard = () => {
           <img src="StatBoard.png" alt="" />
         </div>
         <div className="left-center">
-          <NavLink to="/">Dashboard</NavLink>
+          <NavLink to="/" className="navlink">
+            <img src="Circled Menu.png" />
+            Dashboard
+          </NavLink>
+          <NavLink to="/support" className="navlink">
+            <img src="Support.png" />
+            Support
+          </NavLink>
+          <NavLink to="/plugins" className="navlink">
+            <img src="Puzzle.png" />
+            Plugins
+          </NavLink>
+          <NavLink to="/help" className="navlink">
+            <img src="Help.png" />
+            Help
+          </NavLink>
         </div>
-        <div className="left-bottom"></div>
+        <div className="left-bottom">
+          <p>Logout</p>
+          <img src="Shutdown.png" alt="" />
+        </div>
       </div>
-      <div className="main-right"></div>
+      <div className="main-right">
+        <div className="right-top"></div>
+        <div className="right-center"></div>
+        <div className="right-bottom"></div>
+      </div>
     </>
   );
 };
