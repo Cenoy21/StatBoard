d673e324fb76cf3f215eb86c61549badd01b3c0b
Cenoy21 <ceno7517@gmail.com>
2024-01-23 20:22:39 +0530

    Integrated Material UI Table component and displayed the values from the backend

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,7 +1,38 @@
 import "./table.css";
+import { DataGrid } from "@mui/x-data-grid";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 const Table = () => {
-  return <></>;
+  const [tableData, setTableData] = useState([]);
+
+  const fetchTableData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/api/table");
+      setTableData(response.data);
+    } catch (e) {
+      console.log("Error fetching table data:", e);
+    }
+  };
+
+  useEffect(() => {
+    fetchTableData();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 70 },
+    { field: "name", headerName: "Name", width: 200 },
+    { field: "quantity", headerName: "Quantity", width: 200 },
+    { field: "price", headerName: "Price", width: 200 },
+  ];
+
+  return (
+    <>
+      <div style={{ height: 370, width: "700px" }}>
+        <DataGrid rows={tableData} columns={columns} pageSize={5} />
+      </div>
+    </>
+  );
 };
 
 export default Table;
